57f465e468d7cf909ac0a60b19bfeea02dee60cc
Jesse Nelson <spheromak@gmail.com>
2013-09-26 22:59:55 -0700

    add iptables rule for Direct route on vip

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,6 +9,7 @@
 
 include_recipe "services"
 include_recipe "ktc-utils"
+include_recipe "simple_iptables::default"
 
 iface = KTC::Network.if_lookup "management"
 ip = KTC::Network.address "management"
@@ -19,8 +20,25 @@ member = Services::Member.new node.fqdn,
   port: 3306,
   proto: "tcp",
   ip: ip
-
 member.save
+
+ep = Services::Endpoint.new "mysql"
+ep.load
+
+log "Loaded endpoint #{ep.inspect}"
+
+if ep.ip.empty?
+  log "Endpoint missing IP attribute, moving on"
+  raise
+end
+
+# redirect VIP address to local realserver (DIRECT ROUTE)
+simple_iptables_rule "mysql-direct-route" do
+  table "nat"
+  direction "PREROUTING"
+  rule "-p tcp -d #{ep.ip} --dport 3306 -j REDIRECT"
+  jump false
+end
 
 node.default["openstack"]["db"]["bind_interface"] = iface
 

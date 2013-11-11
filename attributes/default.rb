88c4b01e9c892d7a1eca12531fe6e6d54e1ce303
Sunhee Ahn <itskewpie@gmail.com>
2013-11-11 15:59:20 +0900

    add process monitoring(sensu check and collectd plugin)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,7 @@
 # database to be used
 node.default["openstack"]["db"]["service_type"] = "mysql"
+
+# process monitoring
+default["openstack"]["db"]["service_processes"] = [
+  { "name" =>  "mysqld", "shortname" =>  "mysqld" }
+]
